dcf070c1a940b9e3fa3c9705711ca3e98e477f0f
Felipe da Silva <felipedasilva1825@gmail.com>
2025-01-29 12:21:45 -0300

    criação da base do header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,38 @@
 
 import style from "../header/header.module.css";
 const Header = () =>{
-    <header>
+    return(
         
-    </header>
+        <header>
+            <div>
+                <div>
+                <a>
+                    <h3>
+                        teste
+                    </h3>
+                </a>
+                </div>
+
+                <nav className={style.navUlDiv}>
+                    <ul className={style.navUl}>
+                        <li className={style.navLi}>
+                            <a>Inicio</a>
+                        </li>
+                        <li className={style.navLi}>
+                            <a>Portifolio</a>
+                        </li>
+                        <li className={style.navLi}>
+                            <a>Ajustes</a>
+                        </li>
+                        <li className={style.navLi}>
+                            <a className={style.contatoBtn}>Contato</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>        
+        </header>
+    );
 
 };
 
-export default Header
+export default Header;

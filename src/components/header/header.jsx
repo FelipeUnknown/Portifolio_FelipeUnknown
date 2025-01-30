367bf09f19ac4effd5913264fd52a400e5e8f404
Felipe da Silva <felipedasilva1825@gmail.com>
2025-01-30 14:47:38 -0300

    alinhamento da nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,8 +3,8 @@ import style from "../header/header.module.css";
 const Header = () =>{
     return(
         
-        <header>
-            <div>
+        <header className={style.header}>
+            <div className={style.divPai}>
                 <div>
                 <a>
                     <h3>
@@ -16,13 +16,13 @@ const Header = () =>{
                 <nav className={style.navUlDiv}>
                     <ul className={style.navUl}>
                         <li className={style.navLi}>
-                            <a>Inicio</a>
+                            <a className={style.navBtns}>Inicio</a>
                         </li>
                         <li className={style.navLi}>
-                            <a>Portifolio</a>
+                            <a className={style.navBtns}>Portifolio</a>
                         </li>
                         <li className={style.navLi}>
-                            <a>Ajustes</a>
+                            <a className={style.navBtns}>Ajustes</a>
                         </li>
                         <li className={style.navLi}>
                             <a className={style.contatoBtn}>Contato</a>

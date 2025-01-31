289c14a9ef0be9fc69bcc94e38da9adb9c8b4b49
Felipe da Silva <felipedasilva1825@gmail.com>
2025-01-31 20:12:00 -0300

    Crianção do Hero 31/01/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Header from './components/header/header'
+import Hero from './components/hero/hero'
 
 function App() {
 
   return (
     <>
-      <Header/>      
+      <Header/>
+      <Hero/> 
     </>
   )
 }
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,25 @@
+
+import style from "../hero/hero.module.css";
+import img from "/src/assets/img/ProjetoGemini.png";
+const Hero = () =>{
+    return(
+        <hero className={style.hero}>
+            <section className={style.divSection}>
+                <div className={style.divImg}>
+                    <a href="https://gemini-react-vite.vercel.app">
+                        <img src={img} className={style.IMG}></img>
+                    </a>
+                </div>
+                <div className={style.divText}>
+                    <p>O que é o Lorem Ipsum?
+                    O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.</p>
+                </div>
+
+            </section>
+            <div>
+
+            </div>
+        </hero>
+    );
+}
+export default Hero;

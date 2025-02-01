d69fc3d0c658748196fe8aa0ff73ab14888058fb
Felipe da Silva <felipedasilva1825@gmail.com>
2025-02-01 03:33:23 -0300

    criação da section onde vai ficar a apresentação do projeto Uno 01/02/2025

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 import './App.css'
 import Header from './components/header/header'
 import Hero from './components/hero/hero'
+import Section from './components/section/section'
 
 function App() {
 
   return (
     <>
       <Header/>
-      <Hero/> 
+      <Hero/>
+      <Section/>
     </>
   )
 }
---
+++ src/components/section/section.jsx
@@ -0,0 +1,24 @@
+
+import img from "/src/assets/img/ProjetoUno.png"
+import style from "../section/section.module.css"
+const Section = () =>{
+    return(
+        <section className={style.section}>
+            <div className={style.divText}>
+                <p>
+                    O que é o Lorem Ipsum? O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.
+                </p>
+                <div className={style.divBtn}>
+                    <a className={style.vejaMaisBtn}>Veja mais</a>
+                </div>
+            </div>
+            <div className={style.divImg}>
+                <a>
+                    <img src={img} className={style.img}/>
+                </a>
+            </div>
+
+        </section>
+    );
+};
+export default Section;

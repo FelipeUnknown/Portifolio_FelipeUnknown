03c161e47c4f02aeb2b1e3b4f3d3a07b86a4b94e
Felipe da Silva <felipedasilva1825@gmail.com>
2025-02-08 23:09:44 -0300

    fazer o quadro 08/02/2025

--- src/components/port/port.jsx
+++ src/components/port/port.jsx
@@ -1,10 +1,14 @@
 
+import uno from "/src/assets/img/ProjetoUno.png"
 import gemi from "/src/assets/img/ProjetoGemini.png";
 const Port =()=>{
     return(
         <port>
             <div>
                 <img src={gemi}/>
+            </div>
+            <div>
+                <img src={uno}/>
             </div>
             
         </port>

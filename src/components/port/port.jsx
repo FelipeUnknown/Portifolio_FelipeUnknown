47cd75c9a3015b0883dd35fc5ed20ee926a26e35
Felipe da Silva <felipedasilva1825@gmail.com>
2025-02-09 14:01:38 -0300

    imagens adicionadas e faltando apenas a ProAgency 09/02/2025

--- src/components/port/port.jsx
+++ src/components/port/port.jsx
@@ -1,17 +1,28 @@
-
-import uno from "/src/assets/img/ProjetoUno.png"
+import code from "/src/assets/img/projetoScode.png";
+import uno from "/src/assets/img/ProjetoUno.png";
 import gemi from "/src/assets/img/ProjetoGemini.png";
-const Port =()=>{
-    return(
-        <port>
-            <div>
-                <img src={gemi}/>
-            </div>
-            <div>
-                <img src={uno}/>
-            </div>
-            
-        </port>
-    );
+import style from "../port/port.module.css";
+const Port = () => {
+  return (
+    <port className={style.port}>
+      <section className={style.section}>
+        <div className={style.cardsDiv}>
+          <a href="/">
+            <img src={gemi} className={style.img} />
+          </a>
+        </div>
+        <div className={style.cardsDiv}>
+          <a href="/">
+            <img src={uno} className={style.img} />
+          </a>
+        </div>
+        <div className={style.cardsDiv}>
+          <a href="/">
+            <img src={code} className={style.img} />
+          </a>
+        </div>
+      </section>
+    </port>
+  );
 };
-export default Port ;
+export default Port;

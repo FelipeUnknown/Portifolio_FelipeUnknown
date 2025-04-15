ae532d0f9de06400ac02962e215a4a48dbc0d7d3
Felipe da Silva <felipedasilva1825@gmail.com>
2025-04-15 12:00:07 -0300

    remoção do carrossel 15/04/2025

--- src/components/port/port.jsx
+++ src/components/port/port.jsx
@@ -1,38 +1,10 @@
-import code from "/src/assets/img/projetoScode.png";
-import uno from "/src/assets/img/ProjetoUno.png";
-import gemi from "/src/assets/img/ProjetoGemini.png";
+
 import style from "../port/port.module.css";
-import {motion} from "framer-motion";
-import {useState, useEffect, useRef} from  'react';
+;
 
 const Port = () => {
-  const carousel = useRef();
-  const[width, setWidth] =useState(0)
+  <section className={style.section}>
 
-    useEffect[()=>{
-      setWidth(carousel.current?.scrollWidth - carousel.current?.ofsetWidth);
-    }]
-
-  const images = [code,uno,gemi];
-
-  return (
-    
-    <port className={style.port}>
-      <motion.div  ref={carousel} className={style.carousel} whileTap={{cursor: "grabbing"}}>
-        <motion.div className={style.inner}
-          drag ="x"
-          dragConstraints={{ right: 0 , left: -width }}
-          initial={{x:100}}
-          animate={{x:0}}
-          transition={{ duration:0.8}}
-        >
-          {images. map (image => (<motion.div className={style.item}key={image}>
-            <img src={image} alt="texto alt"/>
-          </motion.div>)) }
-        </motion.div>
-
-      </motion.div>
-    </port>
-  );
+  </section>
 };
 export default Port;

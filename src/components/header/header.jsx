4f891bb7f52762a1e2a343c13259155a700148df
Felipe da Silva <felipedasilva1825@gmail.com>
2025-06-18 11:32:34 -0300

    menu mobile save parcial 18/06/2025

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
 import style from "../header/header.module.css";
+import { link, select } from "framer-motion/client";
+import MenuBars from "../../assets/img/menuBars.svg";
+import MenuClose from "../../assets/img/menuClose.svg";
+
 const Header = () => {
+  const [menuActive, setMenuactive] = useState(false);
+  const links = [
+    {name:"Inicio", link: '/'},
+    {name:"Portifolio", link:'/'},
+    {name:"Ajustes", link:'/'},
+    {name:"Contato", link:'/'}
+  ];
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
   return (
-    <header className={style.header}>
-      <div className={style.divPai}>
+    <header className={`${style.Header}${menuActive ? style.active : ""}`}>
+      <div className={style.divWrapper}>
         <div>
           <a className={style.logoBtn} href="/">
             <h3>FelipeUnknown</h3>
           </a>
         </div>
+        <button id={style.buttonMobile} onClick={toggleMenu}>
+          <img
+            src={menuActive ? MenuClose : MenuBars}
+            id={style.menuIcons}
+            alt="icon menu mobille"
+          />
+        </button>
 
         <nav className={style.navUlDiv}>
-          <ul className={style.navUl}>
+          <ul className={style.navUl} id={style.Menu}>
+            {links.map((link, index) =>(
+              <li className={style.navLi} key={index}><a href="" rel="noreferrer, noopener" className={style.linksMenuLi} key={index}>{link.name}</a></li>
+            ))}
+            <li></li>
             <li className={style.navLi}>
               <a className={style.navBtns} href="/">
                 Inicio
